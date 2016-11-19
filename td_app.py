3a2d2856ef750ecbc871b7c2792b92a0ad032222
ViperXYZ <sammy.haque@mail.utoronto.ca>
2016-11-19 14:20:17 -0500

    added add friend, update password functions

--- td_app.py
+++ td_app.py
@@ -10,8 +10,45 @@ transactions = db.transactions
 def get_userdata(username,collection_name):
     ret_user = collection_name.find_one({"username":username})
     return ret_user
-def add_user(inpt_dict):
-    pass
+
+def debug_table(collection):
+    ret = ""
+    cursor = collection.find({})
+    for document in cursor: 
+        ret += str(document) +"\n"
+    return ret 
+
+def add_user(query, collection):
+    try:
+        rer_user = collection.insert({"username":query["username"],"password":query["password"],"friends":[],"balance":0,"upVote":0,"downVote":0})
+        return "Success"
+    except:
+        return "Failed"
+
+def insert_friend(query,collection):
+    try:
+       ret_usr1 = collection.find_one({"username":query["usr1"]})
+       ret_usr2 =  collection.find_one({"username":query["usr2"]})
+       if not(ret_usr1["_id"] in ret_usr2["friends"]):
+           collection.update({"_id":ret_usr1["_id"]},{"$push":{"friends":ret_usr2["_id"]}})
+           collection.update({"_id":ret_usr2["_id"]},{"$push":{"friends":ret_usr1["_id"]}})
+           return "success"
+       else:
+           return "Failed"
+    except:
+        return "None"
+def update_pass(query,collection):
+    try:
+        collection.update({"username":query["username"]},{"$set":{"password":query["new_password"]}})
+        return "Success"
+    except:
+        return "Failed"
+
+
+def comparePass(query,collection_name):
+    ret_user = collection_name.find_one({"username":query["username"],"password":query["password"]})
+    return str(ret_user != None)
+
 def processTransaction(transactionDict):
     userTo = get_userdata(transactionDict["userToName"], app_users)
     userFrom = get_userdata(transactionDict["userFromName"], app_users)
@@ -37,24 +74,41 @@ def processTransaction(transactionDict):
     
     return "Success"
     
-        
-    
-list_data = app_users.find({})
-for i in list_data:
-    pprint(i)
-
+      
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/addfriend",methods=["POST"])
+def addfriend():
+     json = request.json
+     addfriendverify = insert_friend(json,app_users)
+     return addfriendverify 
 @app.route('/post', methods = ['POST']) 
 def post():
     # Get the parsed contents of the form da
     json = request.json
-    print(type(json))
-    print(str(json))
     usr_data = get_userdata(json["username"],app_users)
     return str(usr_data)
-    
+@app.route('/verify', methods = ['POST'])
+def verify():
+    # Get the parsed contents of the form da
+    json = request.json
+    verified = comparePass(json,app_users)
+    return verified    
+@app.route('/debug',methods = ['POST'])
+def debug():
+    ret = debug_table(app_users)
+    return ret
+@app.route('/changepass', methods = ['POST'])
+def changepass():
+    # Get the parsed contents of the form da
+    json = request.json
+    verified = comparePass(json,app_users)
+    if(verified == "True"):
+        return update_pass(json,app_users)
+    else:
+        return "Original password is not correct"
 @app.route('/transaction', methods = ['POST']) 
 def postTransaction():
     # Get the parsed contents of the form data
