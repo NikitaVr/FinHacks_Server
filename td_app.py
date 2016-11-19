2406e4a4d32203ebfb32318900c01405d08a1fce
ViperXYZ <sammy.haque@mail.utoronto.ca>
2016-11-19 12:02:33 -0500

    added json transmission to app

--- td_app.py
+++ td_app.py
@@ -1,23 +1,17 @@
 from flask import Flask, render_template, request, jsonify
 from pprint import pprint
 from pymongo import MongoClient
+import ast
 app = Flask(__name__)
 client = MongoClient('localhost',27017)
 db = client.FinhacksDB
 app_users = db.appUsers
 transactions = db.transactions
-list_data = app_users.find({})
-for i in list_data:
-    pprint(i)
-app_users.update(
-   {"username":"Bob"},
-   { "$inc": { "balance": -1000} },
-upsert = False
-)
 def get_userdata(username,collection_name):
     ret_user = collection_name.find_one({"username":username})
     return ret_user
-
+def add_user(inpt_dict):
+    pass
 def processTransaction(transactionDict):
     userTo = get_userdata(transactionDict["userToName"], app_users)
     userFrom = get_userdata(transactionDict["userFromName"], app_users)
@@ -51,11 +45,13 @@ for i in list_data:
 
 @app.route("/")
 def index():
-    return "Hello World, send me a POST request"
+    return render_template("index.html")
 @app.route('/post', methods = ['POST']) 
 def post():
-    # Get the parsed contents of the form data
+    # Get the parsed contents of the form da
     json = request.json
+    print(type(json))
+    print(str(json))
     usr_data = get_userdata(json["username"],app_users)
     return str(usr_data)
     
@@ -72,3 +68,4 @@ if __name__ == '__main__':
         host = "0.0.0.0",
         port = 80
     )
+
