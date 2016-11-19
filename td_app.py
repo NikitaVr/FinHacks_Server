b6a4d9961c6d85c7ec0d7bd63933b548960ecb07
NikitaVr <nvarabei@gmail.com>
2016-11-19 05:15:08 -0500

    Added processing of transactions.

--- td_app.py
+++ td_app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 client = MongoClient('localhost',27017)
 db = client.FinhacksDB
 app_users = db.appUsers
+transactions = db.transactions
 list_data = app_users.find({})
 for i in list_data:
     pprint(i)
@@ -13,12 +14,35 @@ app_users.update(
    { "$inc": { "balance": -1000} },
 upsert = False
 )
-def get_userdata(query,collection_name):
-    username = query["username"]
+def get_userdata(username,collection_name):
     ret_user = collection_name.find_one({"username":username})
-    return str(ret_user)
+    return ret_user
 
-
+def processTransaction(transactionDict):
+    userTo = get_userdata(transactionDict["userToName"], app_users)
+    userFrom = get_userdata(transactionDict["userFromName"], app_users)
+    transactions.insert(
+        {"userTo" : userTo["_id"],
+        "userFrom" : userFrom["_id"],
+        "amount" : transactionDict["amount"],
+        "type" : transactionDict["type"],
+        "transactionDate" : transactionDict["transactionDate"],
+        "expectedReturnDate" : transactionDict["expectedReturnDate"]})
+        
+   app_users.update(
+   {"username": transactionDict["userToName"]},
+   { "$inc": { "balance": int(transactionDict["amount"])} },
+        upsert = False
+    )
+    
+   app_users.update(
+   {"username": transactionDict["userFromName"]},
+   { "$inc": { "balance": -int(transactionDict["amount"])} },
+        upsert = False
+    )
+    
+   return "Success"
+    
         
     
 list_data = app_users.find({})
@@ -32,8 +56,16 @@ def index():
 def post():
     # Get the parsed contents of the form data
     json = request.json
-    usr_data = get_userdata(json,app_users)
-    return usr_data
+    usr_data = get_userdata(json["username"],app_users)
+    return str(usr_data)
+    
+@app.route('/transaction', methods = ['POST']) 
+def post():
+    # Get the parsed contents of the form data
+    json = request.json
+    
+    response = processTransaction(json)
+    return response
 
 if __name__ == '__main__':
     app.run(
